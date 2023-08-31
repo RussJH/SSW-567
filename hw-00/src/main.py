6db7fefa81b078843db4b0bd3dda3c16a74fdeba
RussJH <russell.j.harrington@gmail.com>
2023-08-31 14:56:59 -0400

    first assignment

---
+++ hw-00/src/main.py
@@ -0,0 +1,12 @@
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
+
+
+def main():
+    """Main Function"""
+    print('Hello World')
+
+if __name__ == '__main__':
+    main()
